523fa3dfff8dc1849596f854c4e7b47990973e8e
Tuku-Webian <tukuwebian@gmail.com>
2022-11-25 22:06:20 +0600

    Delete product API created

--- index.js
+++ index.js
@@ -97,7 +97,12 @@ async function run() {
       const result = await Products.insertOne(product)
       res.send(result)
     })
-
+    app.delete('/products/:id', verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await Products.deleteOne(query);
+      res.send(result);
+    })
   }
   finally {
 
